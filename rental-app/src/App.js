495171169c95f663060dd688f3efe0c20c0af666
seymakara <seymakara88@gmail.com>
2019-01-27 09:23:42 -0800

    create reantal-app project

---
+++ rental-app/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import Movies from './components/Movies';
+
+class App extends Component {
+  render() {
+    return (
+      <Movies />
+    );
+  }
+}
+
+export default App;

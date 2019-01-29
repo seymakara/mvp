47dc54de9405583c02639a65ddbaa9f24430d1ef
seymakara <seymakara88@gmail.com>
2019-01-29 11:32:17 -0800

    modularize input and button

--- rental-app/src/components/FormTemplate.jsx
+++ rental-app/src/components/FormTemplate.jsx
@@ -7,12 +7,38 @@ class FormTemplate extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
+    this.doSubmit();
   }
 
   handleChange = e => {
     const data = { ...this.state.data };
     data[e.currentTarget.name] = e.currentTarget.value;
     this.setState({ data })
+  }
+
+  renderButton = (label) => {
+    return (
+      <button className="btn btn-primary">Login</button>
+    )
+  }
+
+  renderInput = (name, label, type = 'text') => {
+    const { data } = this.state
+    return (
+      < div className="form-group" >
+        <label htmlFor={name}>{label}</label>
+        <input
+          name={name}
+          type={type}
+          value={data[name]}
+          onChange={this.handleChange}
+          autoFocus
+          id={name}
+          className="form-control"
+          required={true}
+        />
+      </div >
+    )
   }
 
 
--- rental-app/src/components/Login.jsx
+++ rental-app/src/components/Login.jsx
@@ -11,37 +11,14 @@ class Login extends FormTemplate {
   }
 
   render() {
-    const { data } = this.state
     return (
       < div >
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input
-              name='username'
-              value={data.username}
-              onChange={this.handleChange}
-              autoFocus
-              id='username'
-              type="text"
-              className="form-control"
-              required={true}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              name='password'
-              value={data.password}
-              onChange={this.handleChange}
-              id='password'
-              type="password"
-              className="form-control"
-              required={true}
-            />
-          </div>
-          <button className="btn btn-primary">Login</button>
+          {this.renderInput('username', 'Username')}
+          {this.renderInput('password', 'Password', 'password')}
+          {this.renderButton('Login')}
+
         </form>
       </div >
     );
